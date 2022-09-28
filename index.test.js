59b9989178f9df4e6cb86f24c9a602a2a972a823
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-09-28 12:39:58 -0700

    commented out tests

--- index.test.js
+++ index.test.js
@@ -3,16 +3,16 @@ const app = require("./index");
 const request = supertest(app);
 const hello = require("./public/hello.js");
 
-it("Home page", async (done) => {
-  expect(1).toBe(1);
-  done();
-});
+// it("Home page", async (done) => {
+//   expect(1).toBe(1);
+//   done();
+// });
 
-it("works", async (done) => {
-  expect(hello).toBeTruthy();
-  done();
-});
+// it("works", async (done) => {
+//   expect(hello).toBeTruthy();
+//   done();
+// });
 
-afterAll(async (done) => {
-  done();
-});
+// afterAll(async (done) => {
+//   done();
+// });
